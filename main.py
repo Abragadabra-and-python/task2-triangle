eda8e2c352bf71192701bc78404f5354a06144e4
Alexander <77795206+Abragadabra@users.noreply.github.com>
2021-11-09 23:03:21 +0500

    add:main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+rows = int(input('Enter number of rows >>> '))
+spaces = (rows * 2) - 2 # Counting the number of spaces
+
+if rows == 0 or rows < 0:
+	print(ValueError('Are you a fool, how can I draw such a triangle?'))
+else:
+	for i in range(0, rows):
+		for j in range(0, spaces):
+			print(end=' ') # Displaying spaces
+		spaces -= 2
+		for j in range(0, i + 1):
+			print('* ', end='') # Displaying stars
+		print('')
